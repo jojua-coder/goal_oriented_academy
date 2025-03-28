a3d9f2b92149bdd80df89f985c166f9e88f8e3a0
jojua-coder <jojua911@gmail.com>
2025-03-28 23:49:22 +0400

    commit

--- Level 57/Homwork/task1.py
+++ Level 57/Homwork/task1.py
@@ -16,3 +16,36 @@ def update_light(current):
     }
 
     return dict.get(current)
+
+#3 Count of positives / sum of negatives
+def count_positives_sum_negatives(arr):
+    if not arr:
+        return []
+    
+    pc = 0
+    ns = 0
+    
+    for i in arr:
+        if i > 0:
+            pc += 1
+        else:
+            ns += i
+            
+    return [pc, ns]
+
+
+#4 Jenny's secret message
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)
+
+ 
+#5 Abbreviate a Two Word Name
+
+def abbrev_name(name):
+    name = name.upper()
+    name = name.split()
+    name1 = name[0][0]
+    name2 = name[1][0]
+    return f"{name1}.{name2}"
