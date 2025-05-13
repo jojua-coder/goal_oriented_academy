487a231b26ee24920b98dceb5bd896ca1e4ed035
jojua-coder <jojua911@gmail.com>
2025-05-13 21:21:05 +0400

    commit

---
+++ Level 69/Homwork/task1.py
@@ -0,0 +1,17 @@
+# 1Create Phone Number
+def create_phone_number(n):
+    #your code here
+    return   f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
+
+
+#2 Find the odd int
+def find_it(seq):
+    result=[]
+    for i in seq:
+        count=seq.count(i)
+        if count %2==1:
+            result.append(i)
+    return result[0]
+
+
+
